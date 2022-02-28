2f5c6cc64318e4f724c75b132ac6ccaff73d6f56
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-02-28 22:57:18 +0530

    Basic Carousel added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,80 +1,106 @@
 import React, { useState } from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-export default function Carousel({ children, modalOpen: initialModalOpen }) {
-  const [modalOpen, setModalOpen] = useState(initialModalOpen);
-  const add = () => {
-    setCount(i => i + 1);
-    console.log('adding');
-  };
-  const subtract = () => {
-    setCount(i => i - 1);
-    console.log('Subtracting');
-  };
-  const clickEvent = () => {
-    setModalOpen(modalOpen => !modalOpen);
-    console.log('toggling modal open');
-  };
+export default function CarouselComp({ children }) {
+  let cardsObjects = [
+    {
+      tags: ['blog', 'product', 'marketing'],
+      heading: 'Churn Prediction',
+      link: '/blog/churn_prediction',
+      color: 'E79E53',
+      field: 'Finance',
+      description:
+        'Predict when customers are likely to leave and take proactive measures to reduce churn.',
+    },
+    {
+      tags: ['blog', 'sales'],
+      heading: 'Lead Conversion',
+      link: '/blog/lead_conversion',
+      color: 'EA8BE7',
+      description:
+        'Understand which leads are likely to convert for you using power of machine learning.',
+    },
+  ];
 
-  const modalClose = () => {
-    setModalOpen(modalOpen => false);
-    console.log('setting modalOpen as false');
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 5,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 5,
+    },
   };
+  //   const [modalOpen, setModalOpen] = useState(initialModalOpen);
+  //   const add = () => {
+  //     setCount(i => i + 1);
+  //     console.log('adding');
+  //   };
+  //   const subtract = () => {
+  //     setCount(i => i - 1);
+  //     console.log('Subtracting');
+  //   };
+  //   const clickEvent = () => {
+  //     setModalOpen(modalOpen => !modalOpen);
+  //     console.log('toggling modal open');
+  //   };
+
+  //   const modalClose = () => {
+  //     setModalOpen(modalOpen => false);
+  //     console.log('setting modalOpen as false');
+  //   };
 
   return (
     <>
-      <button onClick={clickEvent}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <line
-            x1="9"
-            y1="7"
-            x2="23"
-            y2="7"
-            stroke="#ffffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-          ></line>
-          <line
-            x1="4"
-            y1="15"
-            x2="23"
-            y2="15"
-            stroke="#ffffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-          ></line>
-        </svg>
-      </button>
-      <div
-        id="myModal"
-        className={
-          'h-screen overflow-hidden ' + (modalOpen ? 'flex' : 'hidden')
-        }
-      >
-        <div className="h-full bg-black overflow-hidden modal-content">
-          <div className="flex text-white items-center justify-between text-2xl font-bold">
-            EngageML (beta)
-            <span className="close" onClick={modalClose}>
-              &times;
-            </span>
+      <Carousel responsive={responsive}>
+        {cardsObjects.map((cardData, index) => (
+          <div
+            className="flex flex-col justify-between relative h-auto p-6 m-5 space-y-2 bg-gradient-to-tr from-[#414153] to-[#0A0A0F] rounded-sm  drop-shadow-2xl"
+            key={index}
+          >
+            <div className="pb-5">
+              <p className=" text-white lg:text-lg">{cardData.heading}</p>
+              <p className="font-semibold text-gray-400 lg:text-xl">
+                {cardData.description}
+              </p>
+            </div>
+            <div className="flex justify-start w-full gap-2">
+              {cardData.tags.map((tag, index1) => (
+                <div
+                  className="flex items-center justify-center h-8 p-1 px-2 text-sm text-white bg-[#ffffff4d] rounded-sm capitalize"
+                  key={index1}
+                >
+                  <span>{tag}</span>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="h-screen mt-20 text-center">
-            <a href="/usecases">
-              <p className="mb-5 text-xl text-gray-400">Use Cases</p>
-            </a>
+        ))}
+        <div className="flex flex-col justify-between relative h-auto p-6 m-5 space-y-2 bg-gradient-to-tr from-[#414153] to-[#0A0A0F] rounded-sm  drop-shadow-2xl">
+          <div className="pb-5">
+            <p className=" text-white lg:text-lg">Heading Field</p>
+            <p className="font-semibold text-gray-400 lg:text-xl">
+              Description Field
+            </p>
           </div>
-          <div className="fixed flex flex-col items-center  text-white justify-center w-full mx-auto -ml-5 text-lg tex-center bottom-5">
-            <p>EngageML</p>
+          <div className="flex justify-start w-full gap-2">
+            <div className="flex items-center justify-center h-8 p-1 px-2 text-sm text-white bg-[#ffffff4d] rounded-sm capitalize">
+              <span>Tag Field</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Carousel>
     </>
   );
 }
