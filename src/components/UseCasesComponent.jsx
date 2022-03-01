6f8cf583549b15ca1718460da17dd80343722bfe
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-03-01 20:43:24 +0530

    Completed Usecases page

--- src/components/UseCasesComponent.jsx
+++ src/components/UseCasesComponent.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 export default function UseCasesComponent({ children }) {
   const [selected, setSelected] = useState('blog');
-  cardsObjects = [
+  const cardsObjects = [
     {
       name: 'fraud',
       tags: ['blog', 'finance'],
@@ -68,98 +68,123 @@ export default function UseCasesComponent({ children }) {
         'Predict which employee of yours is likely to leave and take active steps to ensure you retain them',
     },
   ];
+  //   let cardsObjects1 = [];
   return (
     <>
-      <div class="sm:text-center">
-        <h2 class=" text-3xl text-center font-extrabold text-white md:text-5xl lg:text-6xl">
+      <div className="sm:text-center">
+        <h2 className=" text-3xl text-center font-extrabold text-white md:text-5xl lg:text-6xl">
           Use Cases
         </h2>
-        <p class=" mx-auto text-center max-w-2xl pt-4 text-lg lg:text-xl text-white">
+        <p className=" mx-auto text-center max-w-2xl pt-4 text-lg lg:text-xl text-white">
           You don’t need to know coding to understand how data science works!
           See how this AI tool can help you estimate predictions in the
           following sectors.
         </p>
       </div>
-      <div class="justify-center mt-44 pt-28 sm:ml-3 hidden xl:flex">
-        <p class=" text-center text-white font-semibold text-4xl">Sectors</p>
+      <div className="justify-center mt-44 pt-28 sm:ml-3 hidden xl:flex">
+        <p className=" text-center text-white font-semibold text-4xl">
+          Sectors
+        </p>
       </div>
-      <div class="flex-row justify-center pt-7 space-x-12 hidden xl:flex">
+      <div className="flex-row justify-center pt-7 space-x-12 hidden xl:flex">
         <div
-          class={'text-gray-400' + (selected == 'blog' ? 'text-white' : '')}
+          className={'text-gray-400' + (selected == 'blog' ? 'text-white' : '')}
           onClick={() => setSelected('blog')}
         >
-          <span class=" cursor-pointer text-center font-semibold">All</span>
+          <span className=" cursor-pointer text-center font-semibold">All</span>
         </div>
         <div
-          class={'text-gray-400' + (selected == 'support' ? 'text-white' : '')}
+          className={
+            'text-gray-400' + (selected == 'support' ? 'text-white' : '')
+          }
           onClick={() => setSelected('support')}
         >
-          <span class=" cursor-pointer text-center font-semibold">Support</span>
+          <span className=" cursor-pointer text-center font-semibold">
+            Support
+          </span>
         </div>
         <div
-          class={'text-gray-400' + (selected == 'finance' ? 'text-white' : '')}
+          className={
+            'text-gray-400' + (selected == 'finance' ? 'text-white' : '')
+          }
           onClick={() => setSelected('finance')}
         >
-          <span class=" cursor-pointer text-center font-semibold">Finance</span>
+          <span className=" cursor-pointer text-center font-semibold">
+            Finance
+          </span>
         </div>
         <div
-          class={
+          className={
             'text-gray-400' + (selected == 'operations' ? 'text-white' : '')
           }
           onClick={() => setSelected('operations')}
         >
-          <span class=" cursor-pointer text-center font-semibold">
+          <span className=" cursor-pointer text-center font-semibold">
             Operations
           </span>
         </div>
         <div
-          class={'text-gray-400' + (selected == 'product' ? 'text-white' : '')}
+          className={
+            'text-gray-400' + (selected == 'product' ? 'text-white' : '')
+          }
           onClick={() => setSelected('product')}
         >
-          <span class=" cursor-pointer text-center font-semibold">Product</span>
+          <span className=" cursor-pointer text-center font-semibold">
+            Product
+          </span>
         </div>
         <div
-          class={
+          className={
             'text-gray-400' + (selected == 'marketing' ? 'text-white' : '')
           }
           onClick={() => setSelected('marketing')}
         >
-          <span class=" cursor-pointer text-center font-semibold">
+          <span className=" cursor-pointer text-center font-semibold">
             Marketing
           </span>
         </div>
         <div
-          class={'text-gray-400' + (selected == 'sales' ? 'text-white' : '')}
+          className={
+            'text-gray-400' + (selected == 'sales' ? 'text-white' : '')
+          }
           onClick={() => setSelected('sales')}
         >
-          <span class=" cursor-pointer text-center font-semibold">Sales</span>
+          <span className=" cursor-pointer text-center font-semibold">
+            Sales
+          </span>
         </div>
       </div>
       <div className="w-full max-w-screen-2xl mx-auto pt-24 lg:pt-5 space-y-4 flex justify-center flex-col px-2 sm:px-3 lg:px-5">
         <div className="w-full grid gap-5 px-4 lg:px-16 md:grid-cols-2 xl:grid-cols-3 justify-center pb-20 xl:pb-32">
-          {cardsObjects.map((cardData, index) => (
-            <div
-              className="w-full flex flex-col justify-between bg-gradient-to-tr from-[#414153] to-[#0A0A0F] h-auto drop-shadow-md space-y-2 rounded-sm p-6"
-              key={index}
-            >
-              <div className="space-y-2">
-                <p className="lg:text-lg text-white">{cardData.heading}</p>
-                <p className="text-[#636374] font-semibold lg:text-xl">
-                  <span className="text-[#636374]">{cardData.description}</span>
-                </p>
+          {cardsObjects
+            .filter(cardData1 => cardData1.tags.includes(selected))
+            .map((cardData, index) => (
+              <div
+                className="w-full flex flex-col justify-between bg-gradient-to-tr from-[#414153] to-[#0A0A0F] h-auto drop-shadow-md space-y-2 rounded-sm p-6"
+                key={index}
+              >
+                <div className="space-y-2">
+                  <p className="lg:text-lg text-white">{cardData.heading}</p>
+                  <p className="text-[#636374] font-semibold lg:text-xl">
+                    <span className="text-[#636374]">
+                      {cardData.description}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex justify-start w-full gap-2">
+                  {cardData.tags
+                    .filter(tag => tag != 'blog')
+                    .map((tag, index1) => (
+                      <div
+                        className="flex items-center justify-center h-8 p-1 px-2 text-sm text-white bg-[#ffffff4d] rounded-sm capitalize"
+                        key={index1}
+                      >
+                        <span>{tag}</span>
+                      </div>
+                    ))}
+                </div>
               </div>
-              <div className="flex justify-start w-full gap-2">
-                {cardData.tags.map((tag, index1) => (
-                  <div
-                    className="flex items-center justify-center h-8 p-1 px-2 text-sm text-white bg-[#ffffff4d] rounded-sm capitalize"
-                    key={index1}
-                  >
-                    <span>{tag}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
